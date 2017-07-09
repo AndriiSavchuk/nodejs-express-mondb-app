4fa74280e5a6daa113fdb1647e86ac2e61241767
Andrii Savchuk <andriksr@gmail.com>
2017-07-09 18:52:42 +0200

    set basic functions

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hi, folks!');
+const express = require('express');
+
+const app = express();
+
+//Index Route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+//Start Server Function
+app.listen(3000, () => {
+  console.log('Server started on port 3000...')
+});
