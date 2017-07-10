285b26d746817baa36ffb24fee0227f52095f0ed
Andrii Savchuk <andriksr@gmail.com>
2017-07-10 22:04:00 +0200

    added functionality to take data from MongoDB

--- app.js
+++ app.js
@@ -9,49 +9,36 @@ mongoose.connect('mongodb://localhost/nodexpsdb', {
 let db = mongoose.connection;
 
 //Check connection
-db.once('open', function(){
+db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
 //Check DB errors
-db.on('error', function(err) {
+db.on('error', (err) => {
   console.log
 });
 
 //Init App
 const app = express();
 
+//Bring in Models
+let Article = require('./models/article');
+
 //Load View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
 //Index Route
 app.get('/', (req, res) => {
-  let articles = [
-    {
-      id: 1,
-      title: 'Article One',
-      author: 'John Wayne',
-      body: 'This is article one'
-    },
-    {
-      id: 2,
-      title: 'Article Two',
-      author: 'Jane Doe',
-      body: 'This is article two'
-    },
-    {
-      id: 3,
-      title: 'Article Three',
-      author: 'Brad Pitt',
-      body: 'This is article three'
+  Article.find({}, (err, articles) => {
+    if (err) {
+      consol.log(err);
+    } else {
+      res.render('index', {
+        title: 'Articles',
+        articles: articles
+      });
     }
-  ];
-
-  res.render('index', {
-    title: 'Articles',
-    articles: articles
   });
 });
 
