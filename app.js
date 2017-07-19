831963778c18e290e15d75d927925232990b1133
Andrii Savchuk <andriksr@gmail.com>
2017-07-19 17:31:45 +0200

    added fbody-parser and functionality for adding articles to database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 //Connection to MongoDB
 mongoose.connect('mongodb://localhost/nodexpsdb', {
@@ -28,6 +29,14 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+/*Body Parser Middleware */
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Parse application/json
+app.use(bodyParser.json())
+
 //Index Route
 app.get('/', (req, res) => {
   Article.find({}, (err, articles) => {
@@ -46,6 +55,23 @@ app.get('/', (req, res) => {
 app.get('/articles/add', (req, res) => {
   res.render('add_article', {
     title: 'Add Articles'
+  });
+});
+
+// Add Submit POST Route
+app.post('/articles/add', (req, res) => {
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save((err) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
   });
 });
 
