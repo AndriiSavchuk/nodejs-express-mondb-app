0e77bff9ac8be6512412ce502e6b5b1388ad9b28
Andrii Savchuk <andriksr@gmail.com>
2017-07-24 19:13:09 +0200

    added form fields validation

--- app.js
+++ app.js
@@ -105,20 +105,34 @@ app.get('/articles/add', (req, res) => {
 
 // Add Submit POST Route
 app.post('/articles/add', (req, res) => {
-  let article = new Article();
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
+  req.checkBody('title', 'Title is required').notEmpty();
+  req.checkBody('author', 'Author is required').notEmpty();
+  req.checkBody('body', 'Body is required').notEmpty();
 
-  article.save((err) => {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      req.flash('success', 'Article Added');
-      res.redirect('/');
-    }
-  });
+  // Getting Errors
+  let errors = req.validationErrors();
+
+  if (errors) {
+    res.render('add_article', {
+      title: 'Add Article',
+      errors: errors
+    });
+  } else {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save((err) => {
+      if (err) {
+        console.log(err);
+        return;
+      } else {
+        req.flash('success', 'Article Added');
+        res.redirect('/');
+      }
+    });
+  }
 });
 
 // Edit Article
